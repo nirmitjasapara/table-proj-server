ab57ffa7513bba26b6825ce23955afc5a938dce2
nirmit jasapara <nirmit.jasapara@gmail.com>
2021-10-29 16:53:51 -0700

    added downscaling

--- src/time-series/time-series-router.js
+++ src/time-series/time-series-router.js
@@ -14,7 +14,12 @@ timeSeriesRouter.route("/:symbol").get((req, res, next) => {
   // TODO: down sampling of data
   TimeSeriesService.getTimeSeries(req.app.get("db"), req.params.symbol)
     .then(data => {
-      res.json(data.map(serialize));
+      const downscaled = data.map(serialize).filter((d, i) => {
+        const date = new Date(d.datetime);
+        return date.getHours() >= 15 && date.getMinutes() > 50;
+      });
+      console.log(downscaled);
+      res.json(downscaled);
     })
     .catch(next);
 });
